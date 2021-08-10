c846fe205bb354078dcc19ef24face2b85786508
FELIPE JESUS ROSARIO <felipe.uneb@hotmail.com>
2021-08-10 20:51:13 -0300

    Esse aqui tá visualmente mais agradável

---
+++ Cores.js
@@ -0,0 +1,10 @@
+function solucao(cor1, cor2) {if (cor1 === cor2) {console.log(cor1)} 
+                              else if (cor1 === "amarelo" && cor2 === "azul") {console.log("verde")} 
+                              else if (cor2 === "amarelo" && cor1 === "azul") {console.log("verde")}
+                              else if (cor1 === "vermelho" && cor2 === "azul") {console.log("roxo")} 
+                              else if (cor2 === "vermelho" && cor1 === "azul") {console.log("roxo")} 
+                              else if (cor1 === "amarelo" && cor2 === "vermelho") {console.log("laranja")} 
+                              else if (cor2 === "amarelo" && cor1 === "vermelho") {console.log("laranja")} 
+                              
+
+}
